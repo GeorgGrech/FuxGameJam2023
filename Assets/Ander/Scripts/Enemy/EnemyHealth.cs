86f7adbaaf1d11d1319389a7bf6358948cbcb13e
AnderAzzopardi <anderazzopardi@gmail.com>
2023-10-07 00:23:47 +0300

    EnemyHealthBar, Player Defence
    Implemented Enemy Health Bar which stacks to how much health the enemy has, Implemented basic player defense (No invincibilty yet) where the player can enter a defense state and moves slow while defending

--- Assets/Ander/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Ander/Scripts/Enemy/EnemyHealth.cs
@@ -1,21 +1,33 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
     public int enemyMaxHealth = 100;  // Maximum health of the enemy.
     private int enemyCurrentHealth;   // Current health of the enemy.
 
+    [SerializeField] private Image healthBar;
     private void Start()
     {
         // Initialize the current health to the maximum health when the enemy spawns.
         enemyCurrentHealth = enemyMaxHealth;
+        
     }
 
-    // Method to handle taking damage.
+
     public void TakeDamage(int damage)
     {
         // Reduce the current health by the amount of damage.
         enemyCurrentHealth -= damage;
+
+        // Ensure that the health doesn't go below zero.
+        enemyCurrentHealth = Mathf.Max(enemyCurrentHealth, 0);
+
+        // Calculate the new fill amount based on the updated health values.
+        float newFillAmount = (float)enemyCurrentHealth / enemyMaxHealth;
+
+        // Update the health bar's fill amount.
+        healthBar.fillAmount = newFillAmount;
 
         // Check if the enemy's health has dropped to or below zero.
         if (enemyCurrentHealth <= 0)
@@ -25,9 +37,15 @@ public class EnemyHealth : MonoBehaviour
         }
     }
 
+
     // Method to handle enemy death.
     private void Die()
     {
         Destroy(gameObject);
+    }
+
+    public void UpdateHealthBar(float enemyMaxHealth, float enemyCurrentHealth)
+    {
+        healthBar.fillAmount = enemyCurrentHealth / enemyMaxHealth;
     }
 }
--- Assets/Ander/Scripts/Player/Fighter.cs
+++ Assets/Ander/Scripts/Player/Fighter.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class Fighter : MonoBehaviour
 {
     private Animator anim;
-    public GameObject hitbox; // Reference to the hitbox GameObject.
+    public GameObject hitbox;
+    public GameObject defendSphere;
+    public PlayerMoveDemoAnder playerMoveDemoAnder; // Reference to the PlayerMoveDemoAnder script.
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+
     }
 
     [SerializeField] private float attackCooldown = 0.5f;
     private bool canAttack = true;
+    private bool isDefending = false;
     public LayerMask enemyLayer; // Set this in the Inspector to the layer where your enemies are.
 
     // Update is called once per frame
@@ -24,11 +31,21 @@ public class Fighter : MonoBehaviour
         {
             Attack();
         }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Defend();
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            StopDefending();
+        }
     }
 
     void Attack()
     {
-        // Play the attack animation here if you have one.
+        // Play the attack animation
+
 
         // Get the position of the hitbox.
         Vector3 hitboxPosition = hitbox.transform.position;
@@ -38,15 +55,16 @@ public class Fighter : MonoBehaviour
 
         foreach (Collider hitCollider in hitColliders)
         {
-            // Check if the hit object is an enemy (you can use tags or layers to identify enemies).
+            // Check if the hit object is an enemy 
             if (hitCollider.CompareTag("Enemy"))
             {
-                // Deal damage to the enemy (you need to implement this part in your enemy script).
+                // Deal damage to the enemy 
                 EnemyHealth enemyHealth = hitCollider.GetComponent<EnemyHealth>();
                 if (enemyHealth != null)
                 {
                     Debug.Log("Enemy Taking Damage");
-                    enemyHealth.TakeDamage(10); // Adjust the damage amount as needed.
+                    
+                    enemyHealth.TakeDamage(10); 
                 }
             }
         }
@@ -60,5 +78,25 @@ public class Fighter : MonoBehaviour
         canAttack = false;
         yield return new WaitForSeconds(attackCooldown);
         canAttack = true;
+    }
+
+
+     void Defend()
+    {
+        canAttack = false;
+        // Set the character to defend.
+        isDefending = true;
+        playerMoveDemoAnder.DefendingSpeed(); // Call the DefendingSpeed method from PlayerMoveDemoAnder.                                      
+        defendSphere.SetActive(true);// Show the defense sphere.
+    }
+
+    void StopDefending()
+    {
+        // Reset character's state to normal.
+        canAttack = true;
+        isDefending = false;
+        playerMoveDemoAnder.ReturnToNormalSpeed(); // Call the ReturnToNormalSpeed method from PlayerMoveDemoAnder.
+        defendSphere.SetActive(false);// Show the defense sphere.
+
     }
 }
