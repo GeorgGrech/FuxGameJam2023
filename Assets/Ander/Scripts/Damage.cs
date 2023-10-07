d1abcb9cee0bea38c5eaf650af1e2e4d7dd25e34
AnderAzzopardi <anderazzopardi@gmail.com>
2023-10-07 18:33:32 +0300

    HealthFunctionality
    Added Healthbar functionality and basic pickup system

--- Assets/Ander/Scripts/Damage.cs
+++ Assets/Ander/Scripts/Damage.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
 
-public class HealthPowerUpScript : MonoBehaviour
+public class Damage : MonoBehaviour
 {
-    public int healingAmount = 10; // Amount of healing to provide.
+    public string playerTag = "Player"; // The tag of the player GameObject.
+    public int damageAmount = 10; // Amount of damage to inflict.
 
     private void OnTriggerEnter(Collider other)
     {
         // Check if the object that entered the trigger has the player tag.
-        if (other.CompareTag("Player"))
+        if (other.CompareTag(playerTag))
         {
             // Get the Health component of the player (assuming the player has a Health script).
             Health playerHealth = other.GetComponent<Health>();
@@ -15,11 +16,8 @@ public class HealthPowerUpScript : MonoBehaviour
             // Check if the player has a Health component.
             if (playerHealth != null)
             {
-                // Heal the player.
-                playerHealth.Heal(healingAmount);
-
-                // Destroy the health power-up object.
-                Destroy(gameObject);
+                // Inflict damage on the player.
+                playerHealth.TakeDamage(damageAmount);
             }
         }
     }
